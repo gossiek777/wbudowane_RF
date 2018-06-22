fc8e2a01db6104429849d76b77386acc53903d52
gossiek777 <gossiek777@gmail.com>
2018-06-22 07:18:24 +0200

    zadanie 6, nie działa

--- RF_part1/Debug/src/main.d
+++ RF_part1/Debug/src/main.d
@@ -20,7 +20,7 @@ src/main.d src/main.o: ../src/main.c ../src/avr_compiler.h \
  c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
  c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\math.h \
  c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\sleep.h \
- ../src/led.h
+ ../src/led.h ../src/rf.h
 
 ../src/avr_compiler.h:
 
@@ -67,3 +67,5 @@ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-g
 c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1061\avr8-gnu-toolchain\avr\include\avr\sleep.h:
 
 ../src/led.h:
+
+../src/rf.h:
--- RF_part1/src/main.c
+++ RF_part1/src/main.c
@@ -1,29 +1,36 @@
 #include "avr_compiler.h"
 #include "avr/sleep.h"
 #include "led.h"
+#include "rf.h"
 
-#define BOOT_BUTT_bm	3
+#define BOOT_BUTT_bm		3
+#define TRANSCIEVER_INT_PIN	2
+
+unsigned char ucMessage = 0;
 
 ISR(PORTC_INT0_vect){ // interrupt service routine 
+	rf_clear_int_flags();
 	led_toggle();
+	rf_send_byte(ucMessage++);
 };
 
 int main (void){
 	led_init();
 
-	PORTC.DIR = 0;  //input port
-	PORTC.PIN3CTRL= PORT_OPC_PULLUP_gc | PORT_ISC_FALLING_gc;
-	PORTC.INT0MASK=(1<<BOOT_BUTT_bm); 
+	PORTC.DIRCLR = (1<<TRANSCIEVER_INT_PIN);  //set as input pin
+	PORTC.PIN2CTRL=  PORT_ISC_FALLING_gc;
+	PORTC.INT0MASK=(1<<TRANSCIEVER_INT_PIN); 
 	PORTC.INTCTRL= PORT_INT0LVL_LO_gc; 
 	PMIC.CTRL=PMIC_LOLVLEN_bm;	
 	sei(); // global interrupts enabling
 
-	set_sleep_mode(SLEEP_MODE_IDLE); 
-	sleep_enable(); 
+	rf_init_as_tx();
+	rf_clear_int_flags();
+	rf_send_byte(ucMessage++);
 	while(1) {
-		sleep_cpu();
 	}
 }
+
 //pobór pr¹du: 
 //pkt 4:	ok 2.4 mA gdy Led nie œwieci (z zaœwieconym ledem ok 3mA)
 //pkt 5:	ok 1.9 mA gdy Led nie œwieci (z zaœwieconym ledem ok 2.5mA)
