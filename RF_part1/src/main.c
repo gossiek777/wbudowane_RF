e8eff0393aa2f44c2961129d2c53ecba16986257
gossiek777 <gossiek777@gmail.com>
2018-06-22 06:25:24 +0200

    Zadanie 4 ok

--- RF_part1/src/main.c
+++ RF_part1/src/main.c
@@ -12,18 +12,14 @@ int main (void){
 	led_init();
 
 	PORTC.DIR = 0;  //input port
-	PORTC.PIN3CTRL= (1<<1) | (1<<3) | (1<<4); //OPC = 011 ISC = 010
+	PORTC.PIN3CTRL= PORT_OPC_PULLUP_gc | PORT_ISC_FALLING_gc;
 	PORTC.INT0MASK=(1<<BOOT_BUTT_bm); 
-	PORTC.INTCTRL= (1<<0); //Low-level interrupt
-	PMIC.CTRL=(1<<0);	//Low-level Interrupt Enable
+	PORTC.INTCTRL= PORT_INT0LVL_LO_gc; 
+	PMIC.CTRL=PMIC_LOLVLEN_bm;	
 	sei(); // global interrupts enabling
 
-	set_sleep_mode(SLEEP_MODE_IDLE);
-	sleep_enable();
 	while(1) {
-		sleep_cpu();
 	};
 }
 //pobór pr¹du: 
 //pkt 4:	ok 2.4 mA gdy Led nie œwieci (z zaœwieconym ledem ok 3mA)
-//pkt 5:	ok 1.9 mA gdy Led nie œwieci (z zaœwieconym ledem ok 2.5mA)
